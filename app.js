d24bb43fbca6901ec542fc7205485ae1a8c6d373
Amanda Darwisyah <amanda.darwisyah@gmail.com>
2025-04-15 11:12:41 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+// let sections = document.querySelector(".wrapper");
+
+// gsap.to(sections, {
+//   x: "-200vw",
+//   scrollTrigger: {
+//     trigger: ".scroll-container",
+//     start: "top top",
+//     end: "+=5000",
+//     pin: true,
+//     scrub: 2,
+//   },
+// });
